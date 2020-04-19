0ed7fd32736a194b66512e68f29e1fc971226ba0
Jack Park <jackpark@gmail.com>
2020-04-18 17:49:22 -0700

    Added back one library jar

--- pdlExample.sh
+++ pdlExample.sh
@@ -1,1 +1,1 @@
-java -Xms1024M -Xmx4G  -cp ./lib/antlr.jar:./lib/fiola.jar:./lib/hsqldb.jar:./lib/snowball.jar:./target/Pire-1.0.0-SNAPSHOT-jar-with-dependencies.jar de.unidu.is.pdatalog.examples.Example
+java  -cp ./lib/hsqldb.jar:./target/java-unidu-pire-1.0.0-SNAPSHOT-jar-with-dependencies.jar de.unidu.is.pdatalog.examples.Example
